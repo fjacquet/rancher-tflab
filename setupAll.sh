942020d6b1b9fc7f0a3c42db0a7477182981162d
Frederic Jacquet <fred@ljf.ch>
2020-12-10 20:57:45 +0100

    beautifier

--- setupAll.sh
+++ setupAll.sh
@@ -1,4 +1,3 @@
-
 echo "You should be logged in Azure already"
 # Create infra
 terraform apply --auto-approve
@@ -10,11 +9,11 @@ ansible-playbook --private-key=id_rsa -i myazure_rm.yml ans-docker.yml
 rke up --config cluster.yml
 # Get helm setup
 # helm repo add rancher-stable      	https://releases.rancher.com/server-charts/stable
-helm repo add rancher-latest        https://releases.rancher.com/server-charts/latest
-helm repo add jetstack            	https://charts.jetstack.io
-helm repo add prometheus-community	https://prometheus-community.github.io/helm-charts
-helm repo add elastic             	https://helm.elastic.co
-helm repo add bitnami             	https://charts.bitnami.com/bitnami
+helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+helm repo add jetstack https://charts.jetstack.io
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add elastic https://helm.elastic.co
+helm repo add bitnami https://charts.bitnami.com/bitnami
 
 export KUBECONFIG=./kube_config_cluster.yml
 
@@ -23,7 +22,6 @@ kubectl create namespace cattle-system
 helm install rancher rancher-latest/rancher \
   --namespace cattle-system \
   --set hostname=rancher.ljf.home
-
 
 # helm install rancher rancher-latest/rancher \
 #   --namespace cattle-system \
