2b3c9ce6195fad31eb23a82f5fb332530cabc2fb
Frederic Jacquet <fred@ljf.ch>
2020-12-13 19:01:56 +0100

    Dynamic parsing

--- terraform/cluster-rke.tf
+++ terraform/cluster-rke.tf
@@ -1,8 +1,30 @@
 
+locals {
+  mgr_roles    = ["controlplane", "etcd", "worker"]
+  worker_roles = ["worker"]
+}
+
 # Provision RKE cluster on provided infrastructure
 resource "rke_cluster" "rancher_cluster" {
-  depends_on   = [local_file.rke-config, local_file.ssh_private_key_pem]
-  cluster_yaml = file("cluster.yml")
+
+  depends_on            = [azurerm_linux_virtual_machine.k8s]
+  kubernetes_version    = var.rke_kubernetes_version
+  ignore_docker_version = true
+  cluster_name          = "rancher-cluster"
+  ssh_agent_auth        = false
+  ssh_key_path          = local_file.ssh_private_key_pem.filename
+
+  dynamic "nodes" {
+    for_each = azurerm_linux_virtual_machine.k8s
+    content {
+      address          = nodes.value.public_ip_address
+      internal_address = nodes.value.private_ip_address
+      user             = var.vm-user
+      role             = ["controlplane", "etcd", "worker"]
+      ssh_key_path     = local_file.ssh_private_key_pem.filename
+    }
+  }
+
 
   upgrade_strategy {
     drain                  = true
