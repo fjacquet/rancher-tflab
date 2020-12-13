973281c40c32462c14c21859e475799b4e7e5554
Frederic Jacquet <fred@ljf.ch>
2020-12-13 21:31:57 +0100

    improve aset

--- terraform/rg.tf
+++ terraform/rg.tf
@@ -1,5 +1,13 @@
 
 
+resource "random_id" "randomizer" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.main.name
+  }
+  byte_length = 8
+}
+
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
@@ -9,10 +17,12 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_availability_set" "aset-k8s" {
-  name                = "aset-k8s"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
+  name                         = "aset-k8s"
+  location                     = azurerm_resource_group.main.location
+  resource_group_name          = azurerm_resource_group.main.name
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+  managed                      = true
   tags = {
     environment = var.environment
   }
