6c8e61e1fee42e2cae059deaa1e87d017e112f39
Frederic Jacquet <fred@ljf.ch>
2020-12-13 21:29:47 +0100

    create lb a record

--- terraform/network-dnszone.tf
+++ terraform/network-dnszone.tf
@@ -40,3 +40,11 @@ resource "azurerm_dns_a_record" "worker" {
   ttl                 = 300
   target_resource_id  = azurerm_public_ip.worker[count.index].id
 }
+
+resource "azurerm_dns_a_record" "rancher" {
+  name                = "rancher"
+  zone_name           = azurerm_dns_zone.ljf.name
+  resource_group_name = azurerm_resource_group.main.name
+  ttl                 = 300
+  target_resource_id  = azurerm_public_ip.pip-lb.id
+}
