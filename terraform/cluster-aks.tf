e8fb999db79a96fe7ef7df05a93219f0d7c5fb8b
Frederic Jacquet <fred@ljf.ch>
2020-12-13 23:29:16 +0100

    add custom worker

--- terraform/cluster-aks.tf
+++ terraform/cluster-aks.tf
@@ -1,22 +1,19 @@
 
-// resource "azurerm_kubernetes_cluster" "aks-worker" {
-//   name                = "aks-worker"
-//   location            = azurerm_resource_group.main.location
-//   resource_group_name = azurerm_resource_group.main.name
-//   dns_prefix          = "aks-worker"
-
-//   default_node_pool {
-//     name       = "default"
-//     node_count = 1
-//     vm_size    = "Standard_D2_v2"
-//   }
-
-//   identity {
-//     type = "SystemAssigned"
-//   }
-
-//   tags = {
-//     Environment = "Production"
-//   }
-// }
+resource "azurerm_kubernetes_cluster" "aks-worker" {
+  name                = "aks-worker"
+  location                      = azurerm_resource_group.main.location
+  resource_group_name           = azurerm_resource_group.main.name
+  dns_prefix          = "aks-worker"
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  tags = {
+    environment = var.environment
+  }
+}
 
